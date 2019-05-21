45793a8805c2f4fd6791bdeca9b80d682df16490
Oliver Becker <oliver.becker@malighting.de>
2019-05-21 14:14:01 +0200

    First working state of the webcode

---
+++ website/index.js
@@ -0,0 +1,118 @@
+const electronDaemon = require('electron').remote.require('./index.js');
+const sensorMax = 1000;
+const channelCount = 6;
+const channelNames = ["Violett","Blau","Gr&uuml;n","Gelb","Orange","Red"];
+const channelColors = ["purple","blue","green","yellow","orange","red"];
+
+var height = 0;
+
+var barsContainer = null;
+var currMaxDisplay = null;
+var valueContainer = null;
+var comInput = null;
+
+function init()
+{
+    console.log("Init");
+    setInterval(() => {
+        var measures = electronDaemon.getMeasures();
+        if(measures)
+        {
+            console.log(measures);
+            measures = JSON.parse(measures);
+    
+            if(barsContainer)
+            {
+                var box = barsContainer.getBoundingClientRect();
+                height = box.height;
+                var currMax = 0;
+                for(var i = 0; i < measures.length; i++)
+                {
+                    currMax = Math.max(parseInt(measures[i]),currMax);
+                }
+                if(parseInt(currMax) < 10)
+                {
+                    currMax = 10;
+                }
+                currMaxDisplay.innerHTML = currMax;
+
+                valueContainer.innerHTML = "";
+
+                for(var i = 0; i < channelCount; i++)
+                {
+                    barsContainer.childNodes[i].style.height = (parseInt(measures[i])/currMax)*height+"px";
+                    valueContainer.innerHTML += "<label>"+measures[i]+"</label>";
+                }
+
+                comInput.style.color = "";
+            }
+        }
+    },500);
+
+    barsContainer = document.getElementById('bars');
+    valueContainer = document.getElementById('values');
+    currMaxDisplay = document.getElementById('currentMax');
+    var namesContainer = document.getElementById('names');
+
+    for(var i = 0; i < channelCount; i++)
+    {
+        var newBar = document.createElement('div');
+        newBar.className = "bar"
+        newBar.title = channelNames[i];
+        newBar.style.backgroundColor = channelColors[i];
+        barsContainer.appendChild(newBar);
+
+        var newLabel = document.createElement('label');
+        newLabel.innerHTML = channelNames[i];
+        namesContainer.appendChild(newLabel);
+    }
+    
+    var commandInput = document.getElementById('commandInput');
+    commandInput.onkeydown = function(event)
+    {
+        if(event.keyCode == 13)
+        {
+            var input = event.currentTarget;
+            if(input)
+            {
+                electronDaemon.sendCommand(input.value);
+            }
+        }
+    }
+    
+    comInput = document.getElementById('comInput');
+    comInput.value = electronDaemon.getSerialPath();
+    comInput.onkeydown = function(event)
+    {
+        if(event.keyCode == 13)
+        {
+            var input = event.currentTarget;
+            if(input)
+            {
+                electronDaemon.setSerialPath(input.value);
+            }
+        }
+    }
+
+    var ckLED = document.getElementById('ckLED');
+    ckLED.onchange = function(event)
+    {
+        var input = event.currentTarget;
+        if(input)
+        {
+            if(input.checked)
+            {
+                electronDaemon.sendCommand("ATLED1=100");
+            }
+            else
+            {
+                electronDaemon.sendCommand("ATLED1=0");
+            }
+        }
+    }
+}
+
+function portFail()
+{
+    comInput.style.color = "red";
+}
